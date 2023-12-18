4a8596d3d50f6378496543aa7827c42a8a9abc6b
Agnilo <agne.butkute99@gmail.com>
2023-12-18 22:27:47 +0200

    blades

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 /*Route::get('/', function () {
     return view('home');
 });*/
-// Route::get('/', [HomeController::class, 'index']); // Nukreipimas į HomeController index() metodą pagrindiniame puslapyje
-//Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', [HomeController::class, 'index'])->name('home'); // Nukreipimas į HomeController index() metodą pagrindiniame puslapyje
