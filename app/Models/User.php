94a77791b6c5bebb6fc9ff299d4fb4fa17043231
Agnilo <agne.butkute99@gmail.com>
2024-01-07 20:58:36 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
         return $this->belongsToMany(Role::class);
     }
 
-    public function hasAnyRoles($role){
+    public function hasAnyRole($role){
 
         if($this->role()->whereIn('name', $role)->first()){
             return true;
