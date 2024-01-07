e590ce9203ae8d8d7cde620166a9e0b36a8a0677
Agnilo <agne.butkute99@gmail.com>
2024-01-07 21:52:09 +0200

    Update 2023_12_18_183106_create_benefits_table.php

--- database/migrations/2023_12_18_183106_create_benefits_table.php
+++ database/migrations/2023_12_18_183106_create_benefits_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('benefit_name');
             $table->text('description')->nullable();
-            $table->string('picture')->nullable();
+            $table->string('picture',255)->nullable();
             $table->decimal('price', 10,2)->default(0.00);
             $table->timestamps();
         });
