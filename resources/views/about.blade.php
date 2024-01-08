8343b98f4d0e68f1f2be6a28c2fcf1dd6843c0ef
Agnilo <agne.butkute99@gmail.com>
2024-01-08 15:14:15 +0200

    blades

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,7 +10,7 @@
             <p>Mūsų įkūrėjai turėjo viziją: sukurti šiuolaikinę, lanksčią ir naudotojui orientuotą darbuotojų valdymo sistemą, kuri leistų įmonėms geriau suprasti, remti ir skatinti savo komandą. Mes siekiame sukurti harmoningą darbo aplinką, kurioje inovacijos, bendradarbiavimas ir darbuotojų gerovė tampa prioritetu.</p>
             <p>Mūsų kelionė prasidėjo nuo mažų žingsnių, tačiau su kiekvienu etapu mes augome ir tobulėjome. Dabar labiau nei bet kada esame pasiryžę toliau kurti "HarmonyWorks", kad taptumėme jūsų komandos patikimais partneriais, kurie padės pasiekti darbo tikslus, skatins bendradarbiavimą ir leis jūsų darbuotojams pasiekti didžiausią potencialą.</p>
             <p>Sujunkime talentą, kurkime harmoniją!</p>
-            <p style="font-style: italic; text-align: right;"> -HarmonyWorks.</p>
+            <p style="font-style: italic; text-align: right;"> -HarmonyWorks</p>
         </div>
     </div>
 </div>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <div class="home-background-main">
-    Sveiki
+<div class="contact-wrapper">
+        <div class="contact-background" style="background-image: url('{{ asset('images/background3.jpg') }}')"> </div>
+        <div class="contact-text">
+            
+        </div>
+    </div>
 </div>
 @endsection
