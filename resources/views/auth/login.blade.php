afc4d88cbc45d8d95922b52ceafc128118d52b65
Agnilo <agne.butkute99@gmail.com>
2024-01-08 17:37:12 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
+<div class="login-background">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
