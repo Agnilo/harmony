bcf54600429188711b853ab79cbff0c165b802cb
Agnilo <agne.butkute99@gmail.com>
2024-01-08 13:57:06 +0200

    blades

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+<div class="home-background-main">
+    <div class="home-background" style="background-image: url('{{ asset('images/background.jpg') }}')">
+        <div class="home-text">
+            <p>Jūsų komanda. Mūsų harmonija.</p>
+            <p style="font-style: italic; font-size:20px;">-HarmonyWorks</p>
+        </div>
+    </div>
+</div>
+@endsection
