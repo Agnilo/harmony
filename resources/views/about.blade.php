95c197a67e892755dc7e2ae5b6f21d1fda55a83b
Agnilo <agne.butkute99@gmail.com>
2024-01-08 14:34:28 +0200

    styles

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="home-background-main">
     <div class="about-wrapper">
+        <div style="background-image: url('{{ asset('images/background1.jpg') }}')"> </div>
         Sveiki
     </div>
 </div>
