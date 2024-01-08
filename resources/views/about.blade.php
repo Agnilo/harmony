abf16793e13926b7ba739f1f4c8e56e62548bbdc
Agnilo <agne.butkute99@gmail.com>
2024-01-08 15:09:58 +0200

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,7 +9,7 @@
             <p>Esame įsipareigoję sukurti darbo aplinką, kurioje kiekvienas darbuotojas jaučiasi vertinamas ir įtrauktas. HarmonyWorks prasidėjo kaip mažas, bet ryžtingas projektas, siekiant sukurti platformą, kuri sujungtų kasdienes užduotis į vieną.</p>
             <p>Mūsų įkūrėjai turėjo viziją: sukurti šiuolaikinę, lanksčią ir naudotojui orientuotą darbuotojų valdymo sistemą, kuri leistų įmonėms geriau suprasti, remti ir skatinti savo komandą. Mes siekiame sukurti harmoningą darbo aplinką, kurioje inovacijos, bendradarbiavimas ir darbuotojų gerovė tampa prioritetu.</p>
             <p>Mūsų kelionė prasidėjo nuo mažų žingsnių, tačiau su kiekvienu etapu mes augome ir tobulėjome. Dabar labiau nei bet kada esame pasiryžę toliau kurti "HarmonyWorks", kad taptumėme jūsų komandos patikimais partneriais, kurie padės pasiekti darbo tikslus, skatins bendradarbiavimą ir leis jūsų darbuotojams pasiekti didžiausią potencialą.</p>
-            <p>Sujunkime talentą, kurkime harmoniją. -HarmonyWorks.</p>
+            <p style="font-style: italic;">Sujunkime talentą, kurkime harmoniją. -HarmonyWorks.</p>
         </div>
     </div>
 </div>
