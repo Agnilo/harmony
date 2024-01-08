4645918b99310ab9c10641b67e66ef3b840168dc
Agnilo <agne.butkute99@gmail.com>
2024-01-08 15:16:55 +0200

    contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,7 +5,7 @@
 <div class="contact-wrapper">
         <div class="contact-background" style="background-image: url('{{ asset('images/background3.jpg') }}')"> </div>
         <div class="contact-text">
-            
+        <h3>Susisiekime</h3>
         </div>
     </div>
 </div>
