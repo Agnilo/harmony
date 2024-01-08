f7201933f3d24cf7f96adc30c8e8d6b7376af8b4
Agnilo <agne.butkute99@gmail.com>
2024-01-08 20:07:58 +0200

    Update reset.blade.php

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="login-background">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">{{ __('Sukurti naują slaptažodį') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
@@ -13,8 +13,8 @@
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                        <div class="row mb-3 card-body-element">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('El. Pašto adresas') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
@@ -27,8 +27,8 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                        <div class="row mb-3 card-body-element">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Slaptažodis') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
@@ -41,25 +41,25 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
+                        <div class="row mb-3 card-body-element">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Pakartoti slaptažodį') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
-                        <div class="row mb-0">
+                        <div class="row mb-0 card-body-bottom-element">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Atnaujinti slaptažodį') }}
                                 </button>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
-        </div>
+        
     </div>
 </div>
 @endsection
