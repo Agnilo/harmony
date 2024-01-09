dc7988e1fc0cd828e9d1f199f63e9f7e221c06a8
Agnilo <agne.butkute99@gmail.com>
2024-01-09 22:44:01 +0200

    Update RolesAndPermissionSeeder.php

--- database/seeders/RolesAndPermissionSeeder.php
+++ database/seeders/RolesAndPermissionSeeder.php
@@ -9,17 +9,17 @@ class RolesAndPermissionSeeder extends Seeder
 {
     public function run()
     {
-        Permission::create(['name' => 'create-users']);
-        Permission::create(['name' => 'edit-users']);
-        Permission::create(['name' => 'delete-users']);
 
-        Permission::create(['name' => 'create-admin']);
-        Permission::create(['name' => 'edit-admin']);
-        Permission::create(['name' => 'delete-admin']);
+        $permissions = [
+            'create-users', 'edit-users', 'delete-users',
+            'create-admin', 'edit-admin', 'delete-admin',
+            'create-benefit', 'edit-benefit', 'delete-benefit',
+            
+        ];
 
-        Permission::create(['name' => 'create-benefit']);
-        Permission::create(['name' => 'edit-benefit']);
-        Permission::create(['name' => 'delete-benefit']);
+        foreach ($permissions as $permissionName) {
+            Permission::firstOrCreate(['name' => $permissionName]);
+        }
 
         $superUserRole = Role::create(['name' => 'Super User']);
         $adminRole = Role::create(['name' => 'Admin']);
