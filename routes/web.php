5c609199383afae950045cf5ecf3e6f3b268cc0d
Agnilo <agne.butkute99@gmail.com>
2024-01-10 00:56:48 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -44,9 +44,9 @@ Route::middleware(['auth.redirect'])->group(function () {
 
 Route::middleware(['can:edit-users'])->group(function () {
     Route::prefix('admin')->group(function () {
-        Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users.index');
-        Route::get('/admin/users', [UserController::class, 'edit'])->name('admin.users.edit');
-        Route::put('/admin/users}', [UserController::class, 'update'])->name('admin.users.update');
-        Route::delete('/admin/users', [UserController::class, 'destroy'])->name('admin.users.destroy');
+        Route::get('/users', [UserController::class, 'index'])->name('admin.users.index');
+        Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
+        Route::put('/users/{user}', [UserController::class, 'update'])->name('admin.users.update');
+        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
     });
 });
