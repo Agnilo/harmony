7d2afb951fe068f9c626dc14a96257fc54a9a849
Agnilo <agne.butkute99@gmail.com>
2024-01-09 18:48:09 +0200

    Update web.blade.php

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -53,7 +53,7 @@
         <header class="web-topbar">
             <div class="web-topbar-header">
                 <div class="web-topbar-header-title">
-                    Sveiki
+                    {{ Route::currentRouteName() }}
                 </div>
                 <div class="web-topbar-header-userarea">
                     logo ar kažkas
