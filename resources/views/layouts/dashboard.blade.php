c923b2f0f05dc0cdeabfecfd27aeb6127e613008
Agnilo <agne.butkute99@gmail.com>
2024-01-09 16:50:20 +0200

    upd

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>HarmonyWorks</title>
+    
+    <link rel="icon" type="image/x-icon" href="{{ asset('icon.png') }}" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
