0a76b106954549b0bd74647cf2ee44671d04938a
Agnilo <agne.butkute99@gmail.com>
2024-01-09 18:23:44 +0200

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.web')
 
 @section('content')
-    <div class="dashboard-content">
-        <h1>Sveiki, {{ $user->first_name }}!</h1>
-        @auth
-            @if(auth()->check() && (auth()->user()->hasRole('admin') || auth()->user()->hasRole('superuser')))
-            <p>Jūs esate prisijungęs kaip: {{ $roles->name }}</p>
-            @endif
-        @endauth
-        
-    </div>
+<div class="dashboard-content">
+    <h1>Sveiki, {{ $user->first_name }}!</h1>
+    @auth
+    @if(auth()->check() && (auth()->user()->hasRole('admin') || auth()->user()->hasRole('superuser')))
+    <p>Jūs esate prisijungęs kaip: 
+        @foreach($roles as $role)
+            <li>{{ $role->name }}</li>
+        @endforeach
+    </p>
+    @endif
+    @endauth
+
+</div>
 @endsection
