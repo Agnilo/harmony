fb3e9c16967c7367d467e5dbcde4de2003abafc9
Agnilo <agne.butkute99@gmail.com>
2024-01-10 16:57:46 +0200

    Update VerifyUser.php

--- app/Http/Middleware/VerifyUser.php
+++ app/Http/Middleware/VerifyUser.php
@@ -3,11 +3,12 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class VerifyUser
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         if(auth()->user()->is_verified){
             return $next($request);
