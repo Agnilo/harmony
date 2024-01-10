d657d3fb9f574d07f0e71e4135432c70b8dafd0f
Agnilo <agne.butkute99@gmail.com>
2024-01-11 00:41:40 +0200

    Update create.blade.php

--- resources/views/benefits/create.blade.php
+++ resources/views/benefits/create.blade.php
@@ -6,14 +6,46 @@
 
     <form action="{{ route('benefits.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
-        <input type="text" name="benefit_name" placeholder="Benefit Name" required>
-        <textarea name="description" placeholder="Description"></textarea>
+        <div class="benefit-create-group">
+            <label for="benefit_name">
+                Privalumo pavadinimas
+            </label>
+            <input type="text" name="benefit_name" required>
+        </div>
+        <div class="benefit-create-group">
+            <label for="description">
+                Privalumo aprašymas
+            </label>
+            <textarea name="description" placeholder="Description"></textarea>
+        </div>
         <div class="form-group">
-            <label for="picture">Select Picture:</label>
+            <label for="picture">
+                Pasirinkti paveikslėlį
+            </label>
             <input type="file" name="picture" class="form-control-file" id="picture">
         </div>
-        <input type="number" name="price" placeholder="Price" step="0.01" required>
-        <button type="submit">Create Benefit</button>
+        <div class="benefit-create-group">
+            <label for="price">
+                Kaina
+            </label>
+            <input type="number" name="price" step="0.50" required>
+        </div>
+        <div class="benefit-create-show"> 
+            Privalumo turinys naudotojui peržiūrint privalumą
+        </div>
+        <div class="benefit-create-group">
+            <label for="introduction">
+                Santrauka
+            </label>
+            <textarea name="introduction"></textarea>
+        </div>
+        <div class="benefit-create-group">
+            <label for="content">
+                Turinys
+            </label>
+            <textarea name="content"></textarea>
+        </div> 
+        <button type="submit">Sukurti privalumą</button>
     </form>
 </div>
 @endsection
