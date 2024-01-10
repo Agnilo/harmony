087094eb66a8cdc4b3ca8a3d69cd9e7c234159f7
Agnilo <agne.butkute99@gmail.com>
2024-01-10 18:51:18 +0200

    Update benefits.blade.php

--- resources/views/benefits.blade.php
+++ resources/views/benefits.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.web')
 
 @section('content')
+<div class="web-child-content">
     <h1>Privalumai</h1>
-    
+
     @foreach($benefits as $benefit)
-        <div>
-            <h2>{{ $benefit->benefit_name }}</h2>
-            <p>{{ $benefit->description }}</p>
-            <img src="{{ $benefit->picture }}" alt="{{ $benefit->benefit_name }}">
-            <p>Price: {{ $benefit->price }}</p>
-        </div>
+    <div>
+        <h2>{{ $benefit->benefit_name }}</h2>
+        <p>{{ $benefit->description }}</p>
+        <img src="{{ $benefit->picture }}" alt="{{ $benefit->benefit_name }}">
+        <p>Price: {{ $benefit->price }}</p>
+    </div>
     @endforeach
+</div>
 @endsection
