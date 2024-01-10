c25847623864776602df7d5a9ee1720d561b9348
Agnilo <agne.butkute99@gmail.com>
2024-01-10 21:23:03 +0200

    Update benefits.blade.php

--- resources/views/benefits.blade.php
+++ resources/views/benefits.blade.php
@@ -25,6 +25,9 @@
         @foreach($benefits as $benefit)
         <div class="col-md-4 mb-4 benefit-card-bottom-margin">
             <div class="benefit-card">
+                <div class="card-header">
+                    <img src="{{ asset('storage/' . $benefit->picture) }}" alt="{{ $benefit->benefit_name }}" class="img-fluid">
+                </div>
                 <div class="benefit-card-body">
                     <h5 class="benefit-card-title">{{ $benefit->benefit_name }}</h5>
                     <p class="benefit-card-text">{{ $benefit->description }}</p>
