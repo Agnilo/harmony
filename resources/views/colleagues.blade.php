a42b3b0b4262a0bfca4621af3d46002a0f4f9598
Agnilo <agne.butkute99@gmail.com>
2024-01-10 11:41:19 +0200

    Update colleagues.blade.php

--- resources/views/colleagues.blade.php
+++ resources/views/colleagues.blade.php
@@ -2,6 +2,31 @@
 
 @section('content')
 <div class="web-child-content">
-    Sveiki
+<div class="card-body">
+            <a href="{{ route('home') }}"><button type="button" class="btn btn-dark">Pagrindinis</button></a>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Vardas</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Vaidmuo</th>
+                        <th scope="col">Veiksmas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{$user->id}}</th>
+                        <td>{{$user->first_name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>{{implode(', ', $user->roles()->get()->pluck('name')->toArray())}}</td>
+                        <td>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 </div>
 @endsection
