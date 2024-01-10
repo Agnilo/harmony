3240ec5622287184464c976ca81ff8ab4dd350f6
Agnilo <agne.butkute99@gmail.com>
2024-01-11 01:12:57 +0200

    benefits

--- resources/views/benefits/show.blade.php
+++ resources/views/benefits/show.blade.php
@@ -34,27 +34,5 @@
 </div>
 
 
-<div class="row">
-    <div class="col-md-4 mb-4 benefit-show-card-bottom-margin">
-        <div class="benefit-show-card">
-            <div class="benefit-card-header">
-                <img src="{{ asset('storage/' . $benefit->picture) }}" alt="{{ $benefit->benefit_name }}" class="img-fluid benefit-img">
-            </div>
-            <div class="benefit-card-body">
-                <h5 class="benefit-card-title">{{ $benefit->benefit_name }}</h5>
-                <p class="benefit-card-text">{{ $benefit->description }}</p>
-            </div>
-            <div class="benefit-card-footer">
-                @if($benefit->price > 0)
-                <p><span style="font-weight: bold">Kaina:</span> {{ $benefit->price }} €</p>
-                @endif
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-
 </div>
 @endsection
