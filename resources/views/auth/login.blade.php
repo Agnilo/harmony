26ab94a07c9220138c8f033b586a8a6decf4bb9a
Agnilo <agne.butkute99@gmail.com>
2024-01-10 10:39:10 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                         <div class="row mb-3 card-body-element">
                             <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('El. Pašto adresas') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 card-body-element-input">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
@@ -27,7 +27,7 @@
                         <div class="row mb-3 card-body-element">
                             <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Slaptažodis') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-6 card-body-element-input">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
