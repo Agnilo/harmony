0789cae203d9c06fae11794526b475c842c654f6
Agnilo <agne.butkute99@gmail.com>
2024-01-10 19:04:19 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -62,6 +62,8 @@ Route::middleware(['can:edit-users'])->group(function () {
 
 Route::middleware(['can:create-benefit'])->group(function () {
     Route::prefix('superuser')->group(function () {
+        Route::get('/privalumai', [BenefitsController::class, 'index'])->name('benefits.index');
+
         Route::get('/privalumai/sukurti', [BenefitsController::class, 'create'])->name('benefits.create');
         Route::post('/privalumai', [BenefitsController::class, 'store'])->name('benefits.store');
     });
