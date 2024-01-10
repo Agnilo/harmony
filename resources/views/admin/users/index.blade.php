a0a40fb4c89da2c3b2f8bd5e5fe413f4c8c88a4a
Agnilo <agne.butkute99@gmail.com>
2024-01-10 02:10:48 +0200

    Update index.blade.php

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,48 +1,7 @@
 @extends('layouts.web')
 
 @section('content')
-<div class="web-child-content">>
-    <div class="row justify-content-md-center" style="background-color: lightgrey">
-        <br>
-        <br>
-        @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-            <li class="nav-item dropdown links">
-                <a id="navbarDropdown" class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }}
-                </a>
-
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <a class="dropdown-item" href="{{route('admin.users.index')}}">
-                        User Management
-                    </a>
-
-                    <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </div>
-            </li>
-            @else
-            <a href="{{ url('/login') }}">Login</a>
-
-            @if (Route::has('register'))
-            <a href="{{ route('register') }}">Register</a>
-            @endif
-            @endauth
-        </div>
-        @endif
-        <div class="col-md-auto my-font" style="color: white">
-            <h1>CatShop</h1>
-        </div>
-        <br>
-        <br>
-    </div>
+<div class="web-child-content">
     <div class="container">
         <br>
         <div class="row justify-content-md-center">
