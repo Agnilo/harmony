97f33939ef8a51d32b7ae010f082822d01c6aaf0
Agnilo <agne.butkute99@gmail.com>
2024-01-10 11:25:42 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                         @csrf
 
                         <div class="row mb-3 card-body-element">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('El. Pašto adresas') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end card-body-element-input">{{ __('El. Pašto adresas') }}</label>
 
                             <div class="col-md-6 card-body-element-input">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -25,7 +25,7 @@
                         </div>
 
                         <div class="row mb-3 card-body-element">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Slaptažodis') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end card-body-element-input">{{ __('Slaptažodis') }}</label>
 
                             <div class="col-md-6 card-body-element-input">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
