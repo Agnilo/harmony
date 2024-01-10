89712e8d8f9912d8420ecd54e4711dafd018b43f
Agnilo <agne.butkute99@gmail.com>
2024-01-10 11:59:48 +0200

    Update colleagues.blade.php

--- resources/views/colleagues.blade.php
+++ resources/views/colleagues.blade.php
@@ -2,40 +2,18 @@
 
 @section('content')
 <div class="web-child-content">
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Vardas</th>
-                <th scope="col">Email</th>
-                <th scope="col">Vaidmuo</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($users as $user)
-            <tr>
-                <th scope="row">{{$user->id}}</th>
-                <td>{{$user->first_name}}</td>
-                <td>{{$user->email}}</td>
-                <td>{{implode(', ', $user->roles()->get()->pluck('name')->toArray())}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <div class="card-body">
         <div class="row">
             @foreach($users as $user)
             <div class="col-md-3 mb-3">
                 <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $user->first_name }}</h5>
-                        <p class="card-text">Email: {{ $user->email }}</p>
+                    <div class="colleagues-card-body">
+                        <h5 class="colleagues-card-title">{{ $user->first_name }}</h5>
+                        <p class="colleagues-card-text">El. Paštas: {{ $user->email }}</p>
+                        <p class="colleagues-card-text">Pareigos: {{ $user->position }}</p>
                     </div>
                 </div>
             </div>
             @endforeach
         </div>
-
-    </div>
 </div>
 @endsection
