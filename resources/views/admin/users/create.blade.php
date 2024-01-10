02e7931b14eee618dd2e58a09ee6e0cb2aa03d02
Agnilo <agne.butkute99@gmail.com>
2024-01-10 13:56:30 +0200

    Update create.blade.php

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -2,17 +2,35 @@
 
 @section('content')
 <div class="web-child-content">
-<form method="POST" action="{{ route('admin.users.store') }}">
-    @csrf
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name" required>
+    <div class="card-body card-body-index">
+        <form method="POST" action="{{ route('admin.users.store') }}">
+            @csrf
+            <label for="first_name">Vardas:</label>
+            <input type="text" id="first_name" name="first_name" required>
 
-    <label for="email">Email:</label>
-    <input type="email" id="email" name="email" required>
+            <label for="last_name">Pavardė:</label>
+            <input type="text" id="last_name" name="last_name" required>
 
-    <!-- Other input fields as needed -->
+            <label for="email">el. Paštas:</label>
+            <input type="email" id="email" name="email" required>
 
-    <button type="submit" class="btn btn-primary">Create User</button>
-</form>
+            <label for="password">Slaptažodis:</label>
+            <input type="password" id="password" name="password" required>
+
+            <div>
+                <label>Gender</label><br>
+                <input type="radio" id="male" name="gender" value="Vyras">
+                <label for="male">Vyras</label><br>
+
+                <input type="radio" id="female" name="gender" value="Moteris">
+                <label for="female">Moteris</label><br>
+
+                <input type="radio" id="other" name="gender" value="Kita">
+                <label for="other">Kita</label><br>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Sukurti</button>
+        </form>
+    </div>
 </div>
 @endsection
