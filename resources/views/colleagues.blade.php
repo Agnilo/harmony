1b348393c910e1c75a229216b314b1230b5c2245
Agnilo <agne.butkute99@gmail.com>
2024-01-10 18:09:06 +0200

    colleagues

--- resources/views/colleagues.blade.php
+++ resources/views/colleagues.blade.php
@@ -4,7 +4,7 @@
 <div class="web-child-content">
         <div class="row">
             @foreach($users as $user)
-            <div class="col-md-3 mb-3">
+            <div class="col-md-3 mb-3 colleagues-card-bottom-margin">
                 <div class="card">
                     <div class="colleagues-card-body">
                         <h5 class="colleagues-card-title">{{ $user->first_name }}</h5>
