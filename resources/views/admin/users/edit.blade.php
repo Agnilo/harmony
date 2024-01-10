b15b0a9d86859bd4384e972340a77e398527a935
Agnilo <agne.butkute99@gmail.com>
2024-01-10 02:11:59 +0200

    Update edit.blade.php

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -1,131 +1,68 @@
-<!DOCTYPE html>
-<html lang="lt">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>CatShop</title>
+@extends('layouts.web')
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-</head>
-<body>
-<br>
-<br>
-<div class="row justify-content-md-center" style="background-color: lightgrey">
-    <br>
-    <br>
-    @if (Route::has('login'))
-        <div class="top-right links">
-            @auth
-                <li class="nav-item dropdown links">
-                    <a id="navbarDropdown" class="dropdown-toggle" href="#" role="button"
-                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                        {{ Auth::user()->name }}
-                    </a>
-
-                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                            {{ __('Logout') }}
-                        </a>
-
-                        <a class="dropdown-item" href="{{route('admin.users.index')}}">
-                            User Management
-                        </a>
-
-                        <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </li>
-            @else
-                <a href="{{ url('/login') }}">Login</a>
-
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}">Register</a>
-                @endif
-            @endauth
-        </div>
-    @endif
-    <div class="col-md-auto my-font" style="color: white"><h1>CatShop</h1></div>
-    <br>
-    <br>
-</div>
-<div class="container">
-    <br>
-    <div class="row justify-content-md-center">
-        <div class="col-md-auto">
-            <h2><a style="color: #262626">Redaguoti vartotoją {{$user->first_name}}</a></h2>
-        </div>
-    </div>
-    <br>
-    <div class="row justify-content-md-center" style="background-color: lightgrey">
+@section('content')
+<div class="web-child-content">
+    <div class="container">
         <br>
-    </div>
-    <div class="card-body">
-        <form action="{{route('admin.users.update', $user) }}" method="POST">
-
-            <div class="form-group row">
-                <label for="email" class="col-md-2 col-form-label text-md-right">Email</label>
-
-                <div class="col-md-6">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                           name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
-
-                    @error('email')
-                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
-                </div>
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto">
+                <h2><a style="color: #262626">Redaguoti vartotoją {{$user->first_name}}</a></h2>
             </div>
+        </div>
+        <br>
+        <div class="row justify-content-md-center" style="background-color: lightgrey">
+            <br>
+        </div>
+        <div class="card-body">
+            <form action="{{route('admin.users.update', $user) }}" method="POST">
 
-            <div class="form-group row">
-                <label for="first_name" class="col-md-2 col-form-label text-md-right">Vardas</label>
+                <div class="form-group row">
+                    <label for="email" class="col-md-2 col-form-label text-md-right">Email</label>
 
-                <div class="col-md-6">
-                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name"
-                           value="{{ $user->first_name }}" required autofocus>
+                    <div class="col-md-6">
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $user->email }}" required autocomplete="email" autofocus>
 
-                    @error('name')
-                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                    @enderror
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
                 </div>
-            </div>
 
-            @csrf
-            {{method_field('PUT') }}
-            <div class="form-group row">
-                <label for="roles" class="col-md-2 col-form-label text-md-right">Vaidmuo</label>
+                <div class="form-group row">
+                    <label for="first_name" class="col-md-2 col-form-label text-md-right">Vardas</label>
 
-                <div class="col-md-6">
-                    @foreach($roles as $role)
+                    <div class="col-md-6">
+                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ $user->first_name }}" required autofocus>
+
+                        @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                @csrf
+                {{method_field('PUT') }}
+                <div class="form-group row">
+                    <label for="roles" class="col-md-2 col-form-label text-md-right">Vaidmuo</label>
+
+                    <div class="col-md-6">
+                        @foreach($roles as $role)
                         <div class="form-check">
-                            <input type="checkbox" name="roles[]" value="{{ $role->id }}"
-                                   @if($user->roles->pluck('id')->contains($role->id)) checked @endif>
+                            <input type="checkbox" name="roles[]" value="{{ $role->id }}" @if($user->roles->pluck('id')->contains($role->id)) checked @endif>
                             <label>{{ $role->name }}</label>
                         </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
-            </div>
-            <button type="submit" class="btn btn-primary">
-                Update
-            </button>
-        </form>
+                <button type="submit" class="btn btn-primary">
+                    Update
+                </button>
+            </form>
+        </div>
     </div>
 </div>
-<br>
-
-</body>
-</html>
+@endsection
