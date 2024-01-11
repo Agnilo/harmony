25a54456abc887e66c55fc7a9011081983e733c6
Agnilo <agne.butkute99@gmail.com>
2024-01-12 00:16:18 +0200

    aa

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -15,6 +15,38 @@
                 </div>
                 <div class="profile-eidt-employee-content">
                     <div class="profile-edit-employee-content-padding">
+                    <div class="row">
+                        <div class="profile-information">
+                            <div class="profile-information-header">
+                                Asmeninė informacija
+                            </div>
+                            <div>
+                                <div> El. Paštas</div>
+                                <div> {{ $user->email }}</div>
+                            </div>
+                            <div>
+                                <div> Lytis</div>
+                                <div> {{ $user->gender }}</div>
+                            </div>
+                            <div>
+                                <div> Adresas </div>
+                                <div> {{ $user->street_address }}</div>
+                            </div>
+                            <div>
+                                <div> Pašto kodas </div>
+                                <div> {{ $user->zip_code }}</div>
+                            </div>
+                            <div>
+                                <div> Miestas </div>
+                                <div> {{ $user->city }}</div>
+                            </div>
+                            <div>
+                                <div> Šalis </div>
+                                <div> {{ $user->country }}</div>
+                            </div>
+                        </div>
+
+                    </div>
                         
 
                         <h2>Selected Benefits:</h2>
