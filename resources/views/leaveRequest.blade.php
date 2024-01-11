1ecb25621d9da85000e809ff5f62d82803930e69
Agnilo <agne.butkute99@gmail.com>
2024-01-12 01:53:41 +0200

    Update leaveRequest.blade.php

--- resources/views/leaveRequest.blade.php
+++ resources/views/leaveRequest.blade.php
@@ -19,6 +19,46 @@
     </div>
     @endforeach
 
+    <div class="card-body card-body-index">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Pavadinimas</th>
+                    <th scope="col">Atostogų tipas</th>
+                    <th scope="col">Priežastis</th>
+                    <th scope="col">Pradžios data</th>
+                    <th scope="col">Pabaigos data</th>
+                    <th scope="col">Statusas</th>
+                    <th scope="col">Veiksmai</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                @foreach ($leaveRequests as $leaveRequest)
+                <tr>
+                    <th scope="row">{{ $leaveRequest->leaveRequest_name }}</th>
+                    <td>{{ $leaveRequest->leave_type }}</td>
+                    <td>{{ $leaveRequest->reason }}</td>
+                    <td>{{ $leaveRequest->start_date }}</td>
+                    <td>{{ $leaveRequest->end_date }}</td>
+                    <td>{{ $leaveRequest->approval_status }}</td>
+                    <td class="user-buttons">
+                        <a href="{{ route('leaveRequest.edit', $leaveRequest) }}" class="btn btn-primary">
+                            <button type="button" class="btn btn-secondary float-left user-button-inside">Redaguoti</button>
+                        </a>
+
+                        <form action="{{ route('leaveRequest.destroy', $leaveRequest) }}" method="POST" class="float-left">
+                            @csrf
+                            {{ method_field('DELETE') }}
+                            <button type="submit" class="btn btn-danger user-button-inside" onclick="return confirm('Ar tikrai norite ištrinti?')">Ištrinti</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
     <!-- Button to create a new leave request -->
     <a href="{{ route('leaveRequest.create') }}" class="btn btn-success">Create New Leave Request</a>
     <p><a href="{{ route('leaveRequests.approve') }}">Approve Leave Requests</a></p>
