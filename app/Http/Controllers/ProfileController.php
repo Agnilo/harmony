d05ae9270cf99200e6bcea34d64e8bc2e7558f1c
Agnilo <agne.butkute99@gmail.com>
2024-01-11 19:15:05 +0200

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -12,14 +12,15 @@ class ProfileController extends Controller
 
         $user = auth()->user();
         $selectedBenefits = $user->selectedBenefits;
-        
+
         $payroll = optional($user->payroll)->toArray();
 
         $workHours = $payroll ? $payroll['work_hours'] : 0;
         $workDays = $payroll ? $payroll['work_days'] : 0;
         $grossSalary = $payroll ? $payroll['gross'] : 0;
         $netSalary = $payroll ? $payroll['net'] : 0;
+        $info = $payroll ? $payroll['info'] : '';
 
-        return view('profile', compact('user', 'selectedBenefits', 'workHours', 'workDays', 'grossSalary', 'netSalary'));
+        return view('profile', compact('user', 'selectedBenefits', 'workHours', 'workDays', 'grossSalary', 'netSalary', 'info'));
     }
 }
