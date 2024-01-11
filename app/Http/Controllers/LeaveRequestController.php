dca3d2c1a456335a485bf7f1c9e7306bd841082f
Agnilo <agne.butkute99@gmail.com>
2024-01-11 12:29:16 +0200

    Update LeaveRequestController.php

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\LeaveRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LeaveRequestController extends Controller
 {
