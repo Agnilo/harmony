bdaf1671c8d1ab4add93e26292098575baac2ae1
Agnilo <agne.butkute99@gmail.com>
2024-01-12 00:21:30 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,47 +7,49 @@
             <div class="profile-edit-employee">
                 <div class="profile-edit-employee-navbar">
                     <div class="profile-edit-employee-imagesection">
-                    <img src="{{ asset('images/user.jpg') }}" alt="Example Image" class="profile-img">
+                        <img src="{{ asset('images/user.jpg') }}" alt="Example Image" class="profile-img">
                     </div>
                     <div class="profile-edit-employee-tab">
-                    <h5>{{ $user->first_name }} {{ $user->last_name }}</h5>
+                        <h5>{{ $user->first_name }} {{ $user->last_name }}</h5>
                     </div>
                 </div>
                 <div class="profile-eidt-employee-content">
                     <div class="profile-edit-employee-content-padding">
-                    <div class="row">
-                        <div class="profile-information">
-                            <div class="profile-information-header">
-                                Asmeninė informacija
+                        <div class="row">
+                            <div class="profile-information">
+                                <div class="profile-information-header">
+                                    Asmeninė informacija
+                                </div>
+                                <div class="profile-information-content">
+                                    <div class="profile-information-text">
+                                        <div> El. Paštas</div>
+                                        <div> {{ $user->email }}</div>
+                                    </div>
+                                    <div class="profile-information-text">
+                                        <div> Lytis</div>
+                                        <div> {{ $user->gender }}</div>
+                                    </div>
+                                    <div class="profile-information-text">
+                                        <div> Adresas </div>
+                                        <div> {{ $user->street_address }}</div>
+                                    </div>
+                                    <div class="profile-information-text">
+                                        <div> Pašto kodas </div>
+                                        <div> {{ $user->zip_code }}</div>
+                                    </div>
+                                    <div class="profile-information-text">
+                                        <div> Miestas </div>
+                                        <div> {{ $user->city }}</div>
+                                    </div>
+                                    <div class="profile-information-text">
+                                        <div> Šalis </div>
+                                        <div> {{ $user->country }}</div>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <div> El. Paštas</div>
-                                <div> {{ $user->email }}</div>
-                            </div>
-                            <div>
-                                <div> Lytis</div>
-                                <div> {{ $user->gender }}</div>
-                            </div>
-                            <div>
-                                <div> Adresas </div>
-                                <div> {{ $user->street_address }}</div>
-                            </div>
-                            <div>
-                                <div> Pašto kodas </div>
-                                <div> {{ $user->zip_code }}</div>
-                            </div>
-                            <div>
-                                <div> Miestas </div>
-                                <div> {{ $user->city }}</div>
-                            </div>
-                            <div>
-                                <div> Šalis </div>
-                                <div> {{ $user->country }}</div>
-                            </div>
+
                         </div>
 
-                    </div>
-                        
 
                         <h2>Selected Benefits:</h2>
                         <ul>
@@ -59,23 +61,23 @@
                         @extends('layouts.app')
 
 
-                            <div class="row justify-content-center">
-                                <div class="col-md-8">
-                                    <div class="card">
-                                        <div class="card-header">Darbo užmokesčio informacija</div>
+                        <div class="row justify-content-center">
+                            <div class="col-md-8">
+                                <div class="card">
+                                    <div class="card-header">Darbo užmokesčio informacija</div>
 
-                                        <div class="card-body">
-                                            <div>
-                                                <h5>Work Hours: {{ $workHours }}</h5>
-                                                <h5>Work Days: {{ $workDays }}</h5>
-                                                <h5>Gross Salary: {{ $grossSalary }}€</h5>
-                                                <h5>Net Salary: {{ $netSalary }}€</h5>
-                                                <h5>Info: {{ $info }}</h5>
-                                            </div>
+                                    <div class="card-body">
+                                        <div>
+                                            <h5>Work Hours: {{ $workHours }}</h5>
+                                            <h5>Work Days: {{ $workDays }}</h5>
+                                            <h5>Gross Salary: {{ $grossSalary }}€</h5>
+                                            <h5>Net Salary: {{ $netSalary }}€</h5>
+                                            <h5>Info: {{ $info }}</h5>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                        </div>
 
                     </div>
                 </div>
