f005aa668b63ef346b6c02c21db1ff7c4023ce40
Agnilo <agne.butkute99@gmail.com>
2024-01-12 01:57:46 +0200

    Update leaveRequest.blade.php

--- resources/views/leaveRequest.blade.php
+++ resources/views/leaveRequest.blade.php
@@ -1,25 +1,10 @@
 @extends('layouts.web')
 
 @section('content')
-<div class="web-child-content">
-    @foreach ($leaveRequests as $leaveRequest)
-    <p>{{ $leaveRequest->reason }}</p>
-    <p>{{ $leaveRequest->user->first_name }}</p>
-    <p>{{ $leaveRequest->approval_status }}</p>
-    <!-- Add more details as needed -->
-
-    <!-- Buttons for each leave request -->
-    <div class="buttons">
-        <a href="{{ route('leaveRequest.edit', $leaveRequest) }}" class="btn btn-primary">Edit</a>
-        <form action="{{ route('leaveRequest.destroy', $leaveRequest) }}" method="POST" style="display: inline;">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this leave request?')">Delete</button>
-        </form>
-    </div>
-    @endforeach
-
     <div class="card-body card-body-index">
+        <div>
+        <a href="{{ route('leaveRequest.create') }}" class="btn btn-success">Sukurti naują atostogų prašymą</a>
+        </div>
         <table class="table">
             <thead>
                 <tr>
@@ -59,7 +44,6 @@
     </div>
 
     <!-- Button to create a new leave request -->
-    <a href="{{ route('leaveRequest.create') }}" class="btn btn-success">Create New Leave Request</a>
     <p><a href="{{ route('leaveRequests.approve') }}">Approve Leave Requests</a></p>
 </div>
 @endsection
