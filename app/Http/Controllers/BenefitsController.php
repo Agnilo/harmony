a79ababef294f2f2204cc63cf382759f9d438d57
Agnilo <agne.butkute99@gmail.com>
2024-01-11 10:38:41 +0200

    Update BenefitsController.php

--- app/Http/Controllers/BenefitsController.php
+++ app/Http/Controllers/BenefitsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Benefits;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class BenefitsController extends Controller
 {
