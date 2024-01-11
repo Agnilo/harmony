9a3677740a84c05f1c4a5a935afda8eb542ed2a9
Agnilo <agne.butkute99@gmail.com>
2024-01-12 01:20:02 +0200

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -44,29 +44,38 @@
                                 <td class="profile-information-cell">{{ $user->country }}</td>
                             </tr>
                         </table>
+                        <div class="profile-information-header">
+                            <h5>Darbo užmokesčio informacija</h5>
+                        </div>
+                        <table class="profile-information-table">
+                            <tr class="profile-information-row">
+                                <td class="profile-information-cell profile-label">Darbo valandos per savaitę</td>
+                                <td class="profile-information-cell">{{ $workHours }}</td>
+                            </tr>
+                            <tr class="profile-information-row">
+                                <td class="profile-information-cell profile-label">Darbo dienos per savaitę:</td>
+                                <td class="profile-information-cell">{{ $workDays }}</td>
+                            </tr>
+                            <tr class="profile-information-row">
+                                <td class="profile-information-cell profile-label">Bruto užmokestis:</td>
+                                <td class="profile-information-cell">{{ $grossSalary }}€</td>
+                            </tr>
+                            <tr class="profile-information-row">
+                                <td class="profile-information-cell profile-label">Neto Užmokestis</td>
+                                <td class="profile-information-cell">{{ $netSalary }}€</td>
+                            </tr>
+                            <tr class="profile-information-row">
+                                <td class="profile-information-cell profile-label">Informacija</td>
+                                <td class="profile-information-cell">{{ $info }}</td>
+                            </tr>
+
+                        </table>
                         <h2>Selected Benefits:</h2>
                         <ul>
                             @foreach ($selectedBenefits as $benefit)
                             <li>{{ $benefit->benefit_name }}</li>
                             @endforeach
                         </ul>
-                        <div class="row justify-content-center">
-                            <div class="col-md-8">
-                                <div class="card">
-                                    <div class="card-header">Darbo užmokesčio informacija</div>
-
-                                    <div class="card-body">
-                                        <div>
-                                            <h5>Work Hours: {{ $workHours }}</h5>
-                                            <h5>Work Days: {{ $workDays }}</h5>
-                                            <h5>Gross Salary: {{ $grossSalary }}€</h5>
-                                            <h5>Net Salary: {{ $netSalary }}€</h5>
-                                            <h5>Info: {{ $info }}</h5>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
 
                     </div>
                 </div>
