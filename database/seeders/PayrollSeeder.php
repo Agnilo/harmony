732080fe6ed9b35245a6a42f7faa59fc5a4e1105
Agnilo <agne.butkute99@gmail.com>
2024-01-11 19:55:02 +0200

    Update PayrollSeeder.php

--- database/seeders/PayrollSeeder.php
+++ database/seeders/PayrollSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Permission;
-use Spatie\Permission\Models\Role;
+use App\Models\User;
+use App\Models\Payroll;
 
 class PayrollSeeder extends Seeder
 {
