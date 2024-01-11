47f58df167f3b52cc7b18b2d3198579b23d567c6
Agnilo <agne.butkute99@gmail.com>
2024-01-11 19:53:14 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesAndPermissionSeeder::class,
+            PayrollSeeder::class,
             UserTableSeeder::class
         ]);
     }
