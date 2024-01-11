f7486382e78dca23737eabb0edcb91020daf856b
Agnilo <agne.butkute99@gmail.com>
2024-01-11 03:36:05 +0200

    Update show.blade.php

--- resources/views/benefits/show.blade.php
+++ resources/views/benefits/show.blade.php
@@ -31,7 +31,7 @@
                         </div>
                         <div class="benefit-show-button">
                             @auth
-                            <form method="POST" action="{{ route('benefit.select', $benefit->id) }}">
+                            <form method="POST" action="{{ route('benefit.select', $benefits->id) }}">
                                 @csrf
 
                                 @if(auth()->user()->selectedBenefits->contains($benefit->id))
