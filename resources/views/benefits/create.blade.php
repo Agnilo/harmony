ad14c68529a1e656242d83d9fe616889f5edb08f
Agnilo <agne.butkute99@gmail.com>
2024-01-12 03:10:16 +0200

    Update create.blade.php

--- resources/views/benefits/create.blade.php
+++ resources/views/benefits/create.blade.php
@@ -2,50 +2,73 @@
 
 @section('content')
 <div class="web-child-content">
-    <h1>Privalumai</h1>
+    <div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-auto">
+                <h2>Privalumas</h2>
+            </div>
+        </div>
 
-    <form action="{{ route('benefits.store') }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        <div class="benefit-create-group">
-            <label for="benefit_name">
-                Privalumo pavadinimas
-            </label>
-            <input type="text" name="benefit_name" required>
-        </div>
-        <div class="benefit-create-group">
-            <label for="description">
-                Privalumo aprašymas
-            </label>
-            <textarea name="description" placeholder="Description"></textarea>
-        </div>
-        <div class="form-group">
-            <label for="picture">
-                Pasirinkti paveikslėlį
-            </label>
-            <input type="file" name="picture" class="form-control-file" id="picture">
-        </div>
-        <div class="benefit-create-group">
-            <label for="price">
-                Kaina
-            </label>
-            <input type="number" name="price" step="0.50" required>
-        </div>
-        <div class="benefit-create-show"> 
-            Privalumo turinys naudotojui peržiūrint privalumą
-        </div>
-        <div class="benefit-create-group">
-            <label for="introduction">
-                Santrauka
-            </label>
-            <textarea name="introduction"></textarea>
-        </div>
-        <div class="benefit-create-group">
-            <label for="content">
-                Turinys
-            </label>
-            <textarea name="content"></textarea>
-        </div> 
-        <button type="submit">Sukurti privalumą</button>
-    </form>
+        <form action="{{ route('benefits.store') }}" method="POST" enctype="multipart/form-data" class="benefit-create-form">
+            @csrf
+
+            <div class="form-group row">
+                <label for="benefit_name" class="col-md-2 col-form-label text-md-right">Privalumo pavadinimas:</label>
+                <div class="col-md-6">
+                    <input type="text" name="benefit_name" class="form-control" required>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="description" class="col-md-2 col-form-label text-md-right">Privalumo aprašymas:</label>
+                <div class="col-md-6">
+                    <textarea name="description" class="form-control" placeholder="Description"></textarea>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="picture" class="col-md-2 col-form-label text-md-right">Pasirinkti paveikslėlį:</label>
+                <div class="col-md-6">
+                    <input type="file" name="picture" class="form-control-file" id="picture">
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="price" class="col-md-2 col-form-label text-md-right">Kaina:</label>
+                <div class="col-md-6">
+                    <input type="number" name="price" step="0.50" class="form-control" required>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-md-2"></div>
+                <div class="col-md-6 benefit-create-show">
+                    Privalumo turinys naudotojui peržiūrint privalumą
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="introduction" class="col-md-2 col-form-label text-md-right">Santrauka:</label>
+                <div class="col-md-6">
+                    <textarea name="introduction" class="form-control"></textarea>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label for="content" class="col-md-2 col-form-label text-md-right">Turinys:</label>
+                <div class="col-md-6">
+                    <textarea name="content" class="form-control"></textarea>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <div class="col-md-2"></div>
+                <div class="col-md-6">
+                    <button type="submit" class="btn btn-primary">Sukurti privalumą</button>
+                </div>
+            </div>
+
+        </form>
+    </div>
 </div>
 @endsection
