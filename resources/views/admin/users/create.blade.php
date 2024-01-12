473a558773235304df91e1493aeadade883fd4a0
Agnilo <agne.butkute99@gmail.com>
2024-01-12 03:48:35 +0200

    Update create.blade.php

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -13,46 +13,153 @@
             <form method="POST" action="{{ route('admin.users.store') }}" class="card-body-layout-flex">
                 @csrf
                 <div class="form-group row create-row">
-                    <label for="first_name" class="col-md-2 col-form-label text-md-right">Vardas:</label>
+                    <label for="email" class="col-md-2 col-form-label text-md-right">El. Paštas</label>
                     <div class="col-md-6">
-                        <input class="form-control type=" text" id="first_name" name="first_name" required>
+                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                 </div>
 
                 <div class="form-group row create-row">
-                    <label for="last_name" class="col-md-2 col-form-label text-md-right">Pavardė:</label>
+                    <label for="first_name" class="col-md-2 col-form-label text-md-right">Vardas</label>
                     <div class="col-md-6">
-                        <input class="form-control type=" text" id="last_name" name="last_name" required>
+                        <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autofocus>
+                        @error('first_name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                 </div>
 
                 <div class="form-group row create-row">
-                    <label for="email" class="col-md-2 col-form-label text-md-right">El. Paštas:</label>
+                    <label for="last_name" class="col-md-2 col-form-label text-md-right">Pavardė</label>
                     <div class="col-md-6">
-                        <input class="form-control type=" email" id="email" name="email" required>
+                        <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autofocus>
+                        @error('last_name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                 </div>
 
                 <div class="form-group row create-row">
-                    <label for="password" class="col-md-2 col-form-label text-md-right">Slaptažodis:</label>
+                    <label for="password" class="col-md-2 col-form-label text-md-right">Slaptažodis</label>
                     <div class="col-md-6">
-                        <input class="form-control" type="password" id="password" name="password" required>
+                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required>
+                        @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
                     </div>
                 </div>
 
                 <div class="form-group row create-row">
                     <label class="col-md-2 col-form-label text-md-right">Lytis</label><br>
                     <div class="col-md-6">
-                        <input type="radio" id="male" name="gender" value="Vyras">
+                        <input type="radio" id="male" name="gender" value="Vyras" {{ old('gender') == 'Vyras' ? 'checked' : '' }}>
                         <label for="male">Vyras</label><br>
 
-                        <input type="radio" id="female" name="gender" value="Moteris">
+                        <input type="radio" id="female" name="gender" value="Moteris" {{ old('gender') == 'Moteris' ? 'checked' : '' }}>
                         <label for="female">Moteris</label><br>
 
-                        <input type="radio" id="other" name="gender" value="Kita">
+                        <input type="radio" id="other" name="gender" value="Kita" {{ old('gender') == 'Kita' ? 'checked' : '' }}>
                         <label for="other">Kita</label><br>
                     </div>
                 </div>
+
+                <!-- Verified Status Selection -->
+                <div class="form-group row">
+                    <label for="is_verified" class="col-md-2 col-form-label text-md-right">Patvirtintas</label>
+                    <div class="col-md-6">
+                        <select id="is_verified" name="is_verified" class="form-control">
+                            <option value="0" {{ old('is_verified') == 0 ? 'selected' : '' }}>Ne</option>
+                            <option value="1" {{ old('is_verified') == 1 ? 'selected' : '' }}>Taip</option>
+                        </select>
+                    </div>
+                </div>
+
+                <!-- Payroll Information -->
+                <h2>Payroll Information</h2>
+
+                <div class="form-group row create-row">
+                    <label for="work_hours" class="col-md-2 col-form-label text-md-right">Darbo valandos per savaitę</label>
+                    <div class="col-md-6">
+                        <input id="work_hours" type="text" class="form-control @error('work_hours') is-invalid @enderror" name="work_hours" value="{{ old('work_hours') }}" required autofocus>
+                        @error('work_hours')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row create-row">
+                    <label for="work_days" class="col-md-2 col-form-label text-md-right">Darbo dienos per savaitę</label>
+                    <div class="col-md-6">
+                        <input id="work_days" type="text" class="form-control @error('work_days') is-invalid @enderror" name="work_days" value="{{ old('work_days') }}" required autofocus>
+                        @error('work_days')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row create-row">
+                    <label for="overtime" class="col-md-2 col-form-label text-md-right">Viršvalandžiai</label>
+                    <div class="col-md-6">
+                        <input id="overtime" type="text" class="form-control @error('overtime') is-invalid @enderror" name="overtime" value="{{ old('overtime') }}" required autofocus>
+                        @error('overtime')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row create-row">
+                    <label for="gross" class="col-md-2 col-form-label text-md-right">Bruto atlyginimas</label>
+                    <div class="col-md-6">
+                        <input id="gross" type="text" class="form-control @error('gross') is-invalid @enderror" name="gross" value="{{ old('gross') }}" required autofocus>
+                        @error('gross')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row create-row">
+                    <label for="net" class="col-md-2 col-form-label text-md-right">Neto atlyginimas</label>
+                    <div class="col-md-6">
+                        <input id="net" type="text" class="form-control @error('net') is-invalid @enderror" name="net" value="{{ old('net') }}" required autofocus>
+                        @error('net')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row create-row">
+                    <label for="info" class="col-md-2 col-form-label text-md-right">Papildoma informacija</label>
+                    <div class="col-md-6">
+                        <input id="info" type="text" class="form-control @error('info') is-invalid @enderror" name="info" value="{{ old('info') }}" autofocus>
+                        @error('info')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+                </div>
+
                 <div class="user-create-button">
                     <button type="submit" class="btn btn-primary user-create-button-margin-right">
                         Sukurti
@@ -62,7 +169,6 @@
                     </a>
                 </div>
             </form>
-
         </div>
     </div>
 </div>
