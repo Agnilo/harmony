1e779d0c154cc6c8450d18ec81915b60fc253384
Agnilo <agne.butkute99@gmail.com>
2024-01-12 02:11:17 +0200

    Update colleagues.blade.php

--- resources/views/colleagues.blade.php
+++ resources/views/colleagues.blade.php
@@ -2,18 +2,21 @@
 
 @section('content')
 <div class="web-child-content">
-        <div class="row">
-            @foreach($users as $user)
-            <div class="col-md-3 mb-3 colleagues-card-bottom-margin">
-                <div class="colleagues-card">
-                    <div class="colleagues-card-body">
-                        <h5 class="colleagues-card-title">{{ $user->first_name }}</h5>
-                        <p class="colleagues-card-text"><span style="font-weight: bold">El. Paštas:</span> {{ $user->email }}</p>
-                        <p class="colleagues-card-text"><span style="font-weight: bold">Pareigos:</span> {{ $user->position }}</p>
-                    </div>
+    <div class="leaveRequest-header">
+        <h5>Mano atostogų prašymai</h5>
+    </div>
+    <div class="row">
+        @foreach($users as $user)
+        <div class="col-md-3 mb-3 colleagues-card-bottom-margin">
+            <div class="colleagues-card">
+                <div class="colleagues-card-body">
+                    <h5 class="colleagues-card-title">{{ $user->first_name }}</h5>
+                    <p class="colleagues-card-text"><span style="font-weight: bold">El. Paštas:</span> {{ $user->email }}</p>
+                    <p class="colleagues-card-text"><span style="font-weight: bold">Pareigos:</span> {{ $user->position }}</p>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
+    </div>
 </div>
 @endsection
