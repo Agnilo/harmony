9b8a850c2bd7c5b959d8e2f8b09d10a19df2bce4
Agnilo <agne.butkute99@gmail.com>
2024-01-12 05:28:59 +0200

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,8 +29,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
-    //protected $redirectTo = '/autorizacija';
+    //protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/autorizacija';
 
     /**
      * Create a new controller instance.
