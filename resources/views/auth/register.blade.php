fb047c0e9b7b543f87b8f4ab2ec8ce7091991a4c
Agnilo <agne.butkute99@gmail.com>
2024-01-12 04:05:39 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,12 +12,26 @@
                         @csrf
 
                         <div class="row mb-3 card-body-element">
-                            <label for="name" class="col-md-4 col-form-label text-md-end card-body-element-input">{{ __('Vardas') }}</label>
+                            <label for="first_name" class="col-md-4 col-form-label text-md-end card-body-element-input">{{ __('Vardas') }}</label>
 
                             <div class="col-md-6 card-body-element-input">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="first_name" type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name') }}" required autocomplete="first_name" autofocus>
 
-                                @error('name')
+                                @error('first_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3 card-body-element">
+                            <label for="last_name" class="col-md-4 col-form-label text-md-end card-body-element-input">{{ __('Pavardė') }}</label>
+
+                            <div class="col-md-6 card-body-element-input">
+                                <input id="last_name" type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name') }}" required autocomplete="last_name" autofocus>
+
+                                @error('last_name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
