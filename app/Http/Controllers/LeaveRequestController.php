0a7e3362471ae056262b3a110e4b84050f32d17c
Agnilo <agne.butkute99@gmail.com>
2024-01-19 16:41:13 +0200

    Update LeaveRequestController.php

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -58,8 +58,6 @@ class LeaveRequestController extends Controller
             'remarks' => 'nullable|string|max:255',
         ]);
 
-        dd('aš čia');
-
         $filePath = $request->file('file_upload') ? $request->file('file_upload')->store('leaveRequests', 'public') : null;
 
         $payroll = $user->payroll()->latest()->first();
@@ -94,26 +92,36 @@ class LeaveRequestController extends Controller
         foreach ($existingLeaveRequests as $leaveRequest) {
             $startDate = new \DateTime($leaveRequest->start_date);
             $endDate = new \DateTime($leaveRequest->end_date);
-        
+
             $startYear = (int)$startDate->format('Y');
             $startMonth = (int)$startDate->format('m');
             $endYear = (int)$endDate->format('Y');
             $endMonth = (int)$endDate->format('m');
-        
+
+            dd($leaveRequest->leave_type);
+
             if (
                 ($startYear == $payrollYear && $startMonth == $payrollMonth) ||
                 ($endYear == $payrollYear && $endMonth == $payrollMonth) ||
                 ($startYear < $payrollYear && $endYear > $payrollYear)
             ) {
-                switch ($leaveRequest->leave_type) {
-                    case 'paid_leave':
-                        $totalPaidLeaveDays += $leaveRequest->days;
-                        break;
-        
-                    case 'unpaid_leave':
-                        $totalUnpaidLeaveDays += $leaveRequest->days;
-                        break;
+                if ($leaveRequest->leave_type === 'paid_leave') {
+                    $totalPaidLeaveDays += $leaveRequest->days;
+                } 
+
+                elseif ($leaveRequest->leave_type === 'unpaid_leave') {
+                    $totalUnpaidLeaveDays += $leaveRequest->days;
                 }
+
+                // switch ($leaveRequest->leave_type) {
+                //     case 'paid_leave':
+                //         $totalPaidLeaveDays += $leaveRequest->days;
+                //         break;
+
+                //     case 'unpaid_leave':
+                //         $totalUnpaidLeaveDays += $leaveRequest->days;
+                //         break;
+                // }
             }
         }
 
