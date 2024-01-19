4d5654d24c6fffe9df688c6ce03ef199b7377872
Agnilo <agne.butkute99@gmail.com>
2024-01-19 17:42:59 +0200

    Update create.blade.php

--- resources/views/leave/create.blade.php
+++ resources/views/leave/create.blade.php
@@ -12,27 +12,12 @@
         <form method="POST" action="{{ route('leaveRequest.store') }}" enctype="multipart/form-data">
             @csrf
 
-            <!-- Leave Name
-            <div class="form-group">
-                <label for="leaveRequest_name">Pavadinimas</label>
-                <input type="text" id="leaveRequest_name" name="leaveRequest_name" class="form-control" rows="3">
-            </div> -->
-
             <div class="form-group row">
                 <label for="leaveRequest_name" class="col-md-2 col-form-label text-md-right">Pavadinimas</label>
                 <div class="col-md-6">
                     <input type="text" id="leaveRequest_name" name="leaveRequest_name" class="form-control" required>
                 </div>
             </div>
-
-            <!-- Leave Type
-            <div class="form-group">
-                <label for="leave_type">Atostogų tipas</label>
-                <select id="leave_type" name="leave_type" class="form-control">
-                    <option value="paid_leave">Apmokamos atostogos</option>
-                    <option value="unpaid_leave">Neapmokamos atostogos</option>
-                </select>
-            </div> -->
 
             <div class="form-group row">
                 <label for="leave_type" class="col-md-2 col-form-label text-md-right">Atostogų tipas</label>
@@ -44,16 +29,30 @@
                 </div>
             </div>
 
-            <!-- Reason -->
+            <!-- Reason
             <div class="form-group">
                 <label for="reason">Priežastis</label>
                 <textarea id="reason" name="reason" class="form-control" rows="3"></textarea>
+            </div> -->
+
+            <div class="form-group row">
+                <label for="reason" class="col-md-2 col-form-label text-md-right">Priežastis</label>
+                <div class="col-md-6">
+                    <input type="text" id="reason" name="reason" class="form-control" required>
+                </div>
             </div>
 
-            <!-- Start Date -->
+            <!-- Start Date
             <div class="form-group">
                 <label for="start_date">Pradžios data</label>
                 <input type="date" id="start_date" name="start_date" class="form-control">
+            </div> -->
+
+            <div class="form-group row">
+                <label for="start_date" class="col-md-2 col-form-label text-md-right">Pradžios data</label>
+                <div class="col-md-6">
+                    <input type="date" id="start_date" name="start_date" class="form-control" required>
+                </div>
             </div>
 
             <!-- End Date -->
