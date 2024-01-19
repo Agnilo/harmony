1ea792e4aba837070de12f8a2f4956e8870a802c
Agnilo <agne.butkute99@gmail.com>
2024-01-19 15:44:59 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -81,7 +81,7 @@ class User extends Authenticatable
     public function calculateNetSalary($gross, $request, $totalPaidLeaveDays, $totalUnpaidLeaveDays)
     {
 
-        dd($gross, $request->all(), $totalPaidLeaveDays, $totalUnpaidLeaveDays);
+        //dd($gross, $request->all(), $totalPaidLeaveDays, $totalUnpaidLeaveDays);
 
         $net = 0;
 
