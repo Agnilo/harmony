fdbe4ad4a6c88b55c001901c50c7e4fd22ab03db
Agnilo <agne.butkute99@gmail.com>
2024-01-19 13:27:26 +0200

    Update LeaveRequestController.php

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -241,6 +241,8 @@ class LeaveRequestController extends Controller
                     }
                 }
 
+                dd($totalPaidLeaveDays, $totalUnpaidLeaveDays);
+
                 if ($leaveRequest->leave_type === 'paid_leave') {
                     $totalPaidLeaveDays -= $leaveRequest->days;
                 } elseif ($leaveRequest->leave_type === 'unpaid_leave') {
